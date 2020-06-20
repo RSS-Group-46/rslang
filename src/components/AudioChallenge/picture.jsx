d4890e5f9ad885ab6e0c068bb47d1dc0859e804f
Vitali Karpuk <karpuk-job@yandex.ru>
2020-06-20 14:58:12 +0300

    feat: add button and image

--- src/components/AudioChallenge/picture.jsx
+++ src/components/AudioChallenge/picture.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 
 const Picture = ({ img }) => {
-
-
-
-
-
-
   const style = {
     backgroundImage: `url(https://raw.githubusercontent.com/irinainina/rslang-data/master/${img})`,
   };
