692e8301eea633a2b2cf0980480c9b3e61289b92
Vladimir Shevtsov <shevvv@tutanota.com>
2020-06-21 20:11:32 +0300

    feat: add few simple filters to use for api queries

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -2,3 +2,17 @@
 export const TOTAL_GROUPS = 6;
 export const PAGES_PER_GROUP = 30;
 export const WORDS_PER_PAGE = 20;
+
+// api userAggregatedWords query filters
+export const filterByDifficulty = (difficulty) => ({
+  "userWord.difficulty": difficulty
+});
+
+export const excludeByDifficulty = (difficulty) => ({
+  "userWord.difficulty": { $ne: difficulty }
+});
+
+export const easyWords = filterByDifficulty('easy');
+export const hardWords = filterByDifficulty('hard');
+export const notEasyWords = excludeByDifficulty('easy');
+export const notHardWords = excludeByDifficulty('hard');
