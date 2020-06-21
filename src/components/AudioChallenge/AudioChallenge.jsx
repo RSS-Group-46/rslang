5626c3dee053fa46db0d30e564b93b62bf25889f
Vitali Karpuk <karpuk-job@yandex.ru>
2020-06-21 17:19:12 +0300

    feat: autofocus button

--- src/components/AudioChallenge/AudioChallenge.jsx
+++ src/components/AudioChallenge/AudioChallenge.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/void-dom-elements-no-children */
+/* eslint-disable jsx-a11y/tabindex-no-positive */
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Loader from '../Loader/Loader';
