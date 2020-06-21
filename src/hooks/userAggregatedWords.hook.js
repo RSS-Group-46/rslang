58d34492c60c2f0021826664da1a831553d046ec
Vladimir Shevtsov <shevvv@tutanota.com>
2020-06-21 14:49:57 +0300

    feat: add user aggregated words hook

---
+++ src/hooks/userAggregatedWords.hook.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import useAuth from './auth.hook';
+import useHttp from './http.hook';
+
+
+const useUserAggregatedWords = (params) => {
+  const { userId, token } = useAuth();
+  const { group, wordsPerPage, filter, onlyUserWords } = params;
+  const [groupState] = useState(group);
+  const [wordsPerPageState] = useState(wordsPerPage);
+  const [filterState] = useState(filter);
+  const [onlyUserWordsState] = useState(onlyUserWords);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const { loading, request } = useHttp();
+
+  const filterEncoded = encodeURIComponent(JSON.stringify(filterState));
+  const paramsStr = `group=${groupState}&wordsPerPage=${wordsPerPageState}&filter=${filterEncoded}&onlyUserWords=${onlyUserWordsState}`;
+  const url = `https://afternoon-falls-25894.herokuapp.com/Users/${userId}/aggregatedWords?${paramsStr}`;
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    Accept: 'application/json',
+  };
+
+  useEffect(() => {
+    const fetchWords = async () => {
+      try {
+        const res = await request(url, 'GET', null, headers);
+        setData(res);
+        setError(null);
+      } catch (err) {
+        setError(err.message || 'Error get words from API');
+      }
+    };
+
+    fetchWords();
+  }, [url, headers, request]);
+
+
+  return { loading, data, error };
+};
+
+export default useUserAggregatedWords;
