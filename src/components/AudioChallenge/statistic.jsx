692b71f6c07422272122fa36b6194f8101c11610
Vitali Karpuk <karpuk-job@yandex.ru>
2020-06-22 13:37:13 +0300

    feat: statistic game

---
+++ src/components/AudioChallenge/statistic.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import '@fortawesome/fontawesome-free/js/all';
+
+const StatisticAudioChallenge = ({ arrCorrectAnswers, arrErrorAnswers }) => {
+  const arrCorrectFilter = new Set(arrCorrectAnswers);
+  const arrErrorsFilter = new Set(arrErrorAnswers);
+  const handleAudio = (audios) => {
+    const audio = new Audio(
+      `https://raw.githubusercontent.com/irinainina/rslang-data/master/${audios}`,
+    );
+    audio.play();
+  };
+  return (
+    <div className="statistic__audio-challenge">
+      <div className="col-lg-4">
+        <div className="bs-component">
+          <div className="alert alert-dismissible alert-success">
+            <button type="button" className="close" data-dismiss="alert">
+              ×
+            </button>
+            <div>
+              <h4>
+                Errors: <span>{[...arrErrorsFilter].length}</span>
+              </h4>
+              {[...arrErrorsFilter].map((item) => (
+                <>
+                  <div
+                    className="table-danger"
+                    onClick={() => handleAudio(item.audio)}
+                    role="presentation"
+                  >
+                    <i className="fas fa-volume-up" />
+                    {item.word}
+                  </div>
+                </>
+              ))}
+            </div>
+            <div>
+              <h4>
+                I know: <span>{[...arrCorrectFilter].length}</span>
+              </h4>
+              {[...arrCorrectFilter].map((item) => (
+                <div
+                  className="table-success"
+                  onClick={() => handleAudio(item.audio)}
+                  role="presentation"
+                >
+                  <i className="fas fa-volume-up" />
+                  {item.word}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StatisticAudioChallenge;
