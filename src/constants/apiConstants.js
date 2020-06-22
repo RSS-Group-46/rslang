7d2bc0f6a0f0b7fd4f95736683f57b23b23736a8
Vladimir Shevtsov <shevvv@tutanota.com>
2020-06-22 14:22:37 +0300

    refactor: introduce constants for words, http methods

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -2,6 +2,9 @@
 export const TOTAL_GROUPS = 6;
 export const PAGES_PER_GROUP = 30;
 export const WORDS_PER_PAGE = 20;
+// words difficulty
+export const HARD = 'HARD';
+export const EASY = 'EASY';
 
 // api userAggregatedWords query filters
 export const filterByDifficulty = (difficulty) => ({
@@ -13,7 +16,13 @@ export const excludeByDifficulty = (difficulty) => ({
 });
 
 export const ONLY_USER_WORDS = { "userWord": { "$ne": null } }
-export const EASY_WORDS = filterByDifficulty('easy');
-export const HARD_WORDS = filterByDifficulty('hard');
-export const NOT_EASY_WORDS = excludeByDifficulty('easy');
-export const NOT_HARD_WORDS = excludeByDifficulty('hard');
+export const EASY_WORDS = filterByDifficulty(EASY);
+export const HARD_WORDS = filterByDifficulty(HARD);
+export const NOT_EASY_WORDS = excludeByDifficulty(EASY);
+export const NOT_HARD_WORDS = excludeByDifficulty(HARD);
+
+// HTTP METHODS for the http hook, except methods absent in swagger
+export const GET = 'GET';
+export const POST = 'POST';
+export const PUT = 'PUT';
+export const DELETE = 'DELETE';
