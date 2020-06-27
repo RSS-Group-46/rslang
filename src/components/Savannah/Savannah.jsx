5253867de506bd94c5f5ddb37de903e53a3b100f
Mihail Gorelikov <mTeeeur@gmail.com>
2020-06-28 00:43:57 +0300

    fix: rewrite component

--- src/components/Savannah/Savannah.jsx
+++ src/components/Savannah/Savannah.jsx
@@ -1,258 +1,137 @@
-/* eslint-disable react/no-access-state-in-setstate */
-/* eslint-disable react/no-did-update-set-state */
+/* eslint-disable no-console */
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import './Savannah.scss';
-import Queue from '../../utils/Queue';
-
-const lifes = new Queue(['*', '*', '*', '*', '*']);
-const words = new Queue([]);
-
-const getWords = async () => {
-  try {
-    const response = await fetch(`https://afternoon-falls-25894.herokuapp.com/words?page=${Math.round(Math.random() * 29)}&group=${Math.round(Math.random() * 5)}`)
-    const data = await response.json();
-
-    data.forEach((el) => {
-      words.add(el.word)
-    })
-  } catch (e) {
-    // TODO: add notification with error
-  }
-}
-
-getWords();
+import getWords from './words';
 
 class Savannah extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      word: words.next(),
-      lifes: lifes.get(),
-      key: '',
-      gameOver: false,
-      correct: 0,
-      wrong: 0,
-      innerValue: '',
+      keyClassName: '',
+      word: '',
+      translatedWord: '',
+      words: [],
+      translatedWords: [],
+      translatedButtons: [],
+      isMounted: false,
     };
-    this.counter = 2;
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
-  componentDidMount() {
-    document.addEventListener('keydown', (e) => {
-      this.setState({
-        key: e.key,
-      });
+  async componentDidMount() {
+    const { words, translatedWords } = await getWords();
+    document.addEventListener('keydown', this.handleKeyPress);
+    this.setState({
+      word: words.next(),
+      translatedWord: translatedWords.next(),
+      words,
+      translatedWords,
+      isMounted: true,
     });
-
-    document.querySelectorAll('.btn').forEach((el) => {
-      el.addEventListener('click', (e) => {
-        this.setState({
-          innerValue: e.target.textContent,
-        });
-      });
-    });
-
-    this.setTimer();
+    this.randomizeWords();
   }
 
   componentDidUpdate() {
-    if (
-      this.state.lifes &&
-      this.state.lifes.length > 0 &&
-      this.counter % 2 === 0 &&
-      this.state.word
-    ) {
-      this.compare(this.state.key);
-      this.counter += 1;
-    } else {
-      this.counter += 1;
-    }
+    const { keyClassName, translatedWord } = this.state;
 
-    if (
-      (this.state.lifes.length === 0 && !this.state.gameOver) ||
-      (!this.state.word && !this.state.gameOver)
-    ) {
-      this.setState({
-        gameOver: true,
-      });
+    if (keyClassName !== 0 && keyClassName.length > 0) {
+      const buttonValue = this.getButtonValue(keyClassName);
+      this.compare(buttonValue, translatedWord);
     }
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', (e) => {
-      this.setState({
-        key: e.key,
-      });
-    });
-
-    document.querySelectorAll('.btn').forEach((el) => {
-      el.removeEventListener('click', (e) => {
-        this.setState({
-          innerValue: e.target.textContent,
-        });
-      });
-    });
-
-    this.resetTimer();
-  }
-
-  getKeyClassName(key) {
+  setKey(key) {
     switch (key) {
       case '1':
-        return `.btn-${key}`;
+        return '.btn-first__value';
 
       case '2':
-        return `.btn-${key}`;
+        return '.btn-second__value';
 
       case '3':
-        return `.btn-${key}`;
+        return '.btn-third__value';
 
       case '4':
-        return `.btn-${key}`;
+        return '.btn-fourth__value';
 
       default:
         return 0;
     }
   }
 
-  setTimer() {
-    this.timer = setInterval(() => {
-      this.setState({
-        word: words.next(),
-      });
-    }, 10000);
+  getButtonValue(className) {
+    const value = document.querySelector(className).textContent;
+    return value;
   }
 
-  resetTimer() {
-    clearInterval(this.timer);
+  compare(buttonValue, currentWord) {
+    const { words, translatedWords } = this.state;
 
-    document.querySelector('.drop').style.webkitAnimation = 'none';
-
-    this.timer = setInterval(() => {
+    if (currentWord.toLowerCase() === buttonValue.toLowerCase()) {
+      const word = words.next();
+      const translatedWord = translatedWords.next();
       this.setState({
-        word: words.next(),
+        word,
+        translatedWord,
       });
-    }, 10000);
-
-    if (this.state.word) {
-      setTimeout(() => {
-        if (document.querySelector('.drop')) {
-          document.querySelector('.drop').style.webkitAnimation = '';
-        }
-      }, 1000);
+      console.log(this.state);
+      this.randomizeWords();
+    } else {
+      // console.log('bad');
     }
   }
 
-  compare(value) {
-    let keyValue;
+  randomizeWords() {
+    const { translatedWords, translatedWord } = this.state;
+    const translatedButtons = [];
+    const correctWordIndex = Math.floor(Math.random() * 4);
 
-    const className = this.getKeyClassName(value);
+    console.log(this.state);
 
-    if (className !== 0 && this.state.key && this.state.key.length > 0) {
-      keyValue = document.querySelector(className).textContent.toLowerCase();
+    for (let i = 0; i < 4; i += 1) {
+      const index = Math.round(
+        Math.random() * (translatedWords.getLength() - 1),
+      );
+
+      if (i === correctWordIndex) {
+        translatedButtons.push(translatedWord);
+      } else {
+        translatedButtons.push(translatedWords.getItem(index));
+      }
     }
 
-    if (this.state.innerValue && this.state.innerValue.length > 0) {
-      keyValue = this.state.innerValue;
-    }
+    this.setState({
+      translatedButtons,
+    });
+  }
 
-    if (
-      (this.state.key &&
-        this.state.key.length > 0 &&
-        keyValue === this.state.word.toLowerCase()) ||
-      (this.state.innerValue &&
-        this.state.innerValue.length > 0 &&
-        keyValue === this.state.word.toLowerCase())
-    ) {
-      this.resetTimer();
+  compenntWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
 
-      const newCorrect = this.state.correct + 1;
-
-      this.setState({
-        prevWord: this.state.word,
-        word: words.next(),
-        key: '',
-        correct: newCorrect,
-        innerValue: '',
-      });
-    } else if (
-      (keyValue !== this.state.word &&
-      this.state.word !== this.state.prevWord &&
-      typeof keyValue !== 'undefined') ||
-      !!keyValue
-    ) {
-      this.resetTimer();
-      lifes.remove();
-
-      const newWrong = this.state.wrong + 1;
-
-      this.setState({
-        lifes: lifes.get(),
-        wrong: newWrong,
-      });
-    }
+  handleKeyPress(e) {
+    const keyClassName = this.setKey(e.key);
+    this.setState({
+      keyClassName,
+    });
   }
 
   render() {
+    const { word, translatedButtons } = this.state;
     return (
       <div className="container">
-        {this.state.gameOver === true ? (
-          <div>
-            <h1>Game over! {this.state.lifes.length === 0 ? '=(' : '=)'}</h1>
-            <h2>Statistics:</h2>
-            <h3>
-              Corret answers: {this.state.correct} (
-              {Math.floor(
-                (this.state.correct / (this.state.wrong + this.state.correct)) *
-                  100,
-              ) || 0}
-              %)
-            </h3>
-            <h3>
-              Wrong answers: {this.state.wrong} (
-              {Math.floor(
-                (this.state.wrong / (this.state.wrong + this.state.correct)) *
-                  100,
-              ) || 0}
-              %)
-            </h3>
-          </div>
-        ) : (
-          <>
-            <div>
-              <button className="btn btn-warning btn-1" type="button">
-                hello
-              </button>
-              <button className="btn btn-warning btn-2" type="button">
-                world
-              </button>
-            </div>
-            <div>
-              <button className="btn btn-warning btn-3" type="button">
-                this
-              </button>
-              <button className="btn btn-warning btn-4" type="button">
-                is
-              </button>
-            </div>
-            <div>
-              {this.state.lifes.map((el) => {
-                return (
-                  <FontAwesomeIcon
-                    key={el + Math.random()}
-                    icon={faStar}
-                    className="m-2"
-                  />
-                );
-              })}
-            </div>
-            <div>
-              <h1 className="drop">{this.state.word}</h1>
-            </div>
-          </>
-        )}
+        <span>{word}</span>
+        <button className="btn btn-outline-danger" type="button">
+          1 - <span className="btn-first__value">{translatedButtons[0]}</span>
+        </button>
+        <button className="btn btn-outline-danger" type="button">
+          2 - <span className="btn-second__value">{translatedButtons[1]}</span>
+        </button>
+        <button className="btn btn-outline-danger" type="button">
+          3 - <span className="btn-third__value">{translatedButtons[2]}</span>
+        </button>
+        <button className="btn btn-outline-danger" type="button">
+          4 - <span className="btn-fourth__value">{translatedButtons[3]}</span>
+        </button>
       </div>
     );
   }
