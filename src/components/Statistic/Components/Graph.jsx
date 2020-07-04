7eb79dea08909823e854f5f31be132b33e1b8a7f
grajoz <grajoz@yandex.ru>
2020-07-04 20:12:36 +0400

    fix:logic

--- src/components/Statistic/Components/Graph.jsx
+++ src/components/Statistic/Components/Graph.jsx
@@ -6,7 +6,9 @@ const arrWordCount = [7,10,5,12,10,10,5,3,8,7,6,12]
 const allWordCount = arrWordCount.reduce(function(item, current) {
     return item + current
   });
- const  horizontalGridLines = 5;
+ // const  horizontalGridLines = 5;
+
+ /*
 const minUnitGraph = () =>{
    let minNumberhorizontalGrid = allWordCount / horizontalGridLines
    let i=0;
@@ -15,7 +17,7 @@ const minUnitGraph = () =>{
     i+=1;
    }
    return Math.round(minNumberhorizontalGrid) * 10 ** i;
-}
+} */
 
 
 
@@ -37,14 +39,19 @@ const getDadaForExperement = () =>{
     return arrData.reverse()
 }
 
+function wordsOfAnyText (x) {
+    // y = -3E-19x6 + 5E-15x5 - 3E-11x4 + 1E-07x3 - 0,0002x2 + 0,1699x
+    return -3*(10**(-19))*x**6 + 5*(10**(-15))*x**5 - 3*(10**(-11))*x**4 + (10**(-7))*x**3  - 0.0002*x**2 + 0.1699*x;
+}
+
+
 const heightGraph = 400;
 const widthGraph = 700;
 
 const Graph = () => {
     const canvasRef = React.useRef(null);
 
-
-    function drawGraph () {
+        function drawGraph () {
             const canvas = canvasRef.current;
             const context = canvas.getContext('2d');  
 
@@ -55,15 +62,25 @@ const Graph = () => {
                 context.lineTo(widthGraph, heightGraph);
                 context.lineTo(0, heightGraph);
                 context.lineTo(0, 0);
-                let coordPasteTextY = 0
-                for (let i=0; i<allWordCount; i += minUnitGraph()){
-  
-                   context.fillText(coordPasteTextY, 2, heightGraph-2- coordPasteTextY/allWordCount*heightGraph); 
-                   context.moveTo(0, heightGraph - coordPasteTextY/allWordCount*heightGraph);
-                   context.lineTo(widthGraph, heightGraph - coordPasteTextY/allWordCount*heightGraph);
-                   coordPasteTextY += minUnitGraph();
+               
+                for (let i=0; i<=100; i += 20){
+                   context.fillText(i + " %", 3, heightGraph-3- i/100*heightGraph*0.9-20); 
+                   context.moveTo(0, heightGraph - i/100*heightGraph*0.9-20);
+                   context.lineTo(widthGraph, heightGraph - i/100*heightGraph*0.9-20);
                 }
-                
+
+                for (let i=0; i<=5000; i += 1000){
+                    context.fillText(i, 35 + i/5000*widthGraph*0.9 , heightGraph-5); 
+                    context.moveTo(35 + i/5000*widthGraph*0.9, heightGraph-20);
+                    context.lineTo(35 + i/5000*widthGraph*0.9, 0);
+                 }
+                context.stroke();
+
+                context.beginPath();
+                context.moveTo(0 + 35, heightGraph-20);
+                for (let i=0; i<=5000; i += 1) {
+                   context.lineTo(35 + i/5000*widthGraph*0.9, heightGraph-20 - wordsOfAnyText (i)/100*heightGraph*0.9);
+                }
                 context.stroke();
 
                 context.beginPath();
@@ -83,7 +100,7 @@ const Graph = () => {
                 context.onmouseover = function () {
                     context.color = 'red'
                 }
-    }
+        }
     
     useEffect(() => {
         drawGraph ()
@@ -91,7 +108,7 @@ const Graph = () => {
     
     return (
             <>
-                <canvas ref={canvasRef} width={700} height={420} />
+                <canvas ref={canvasRef} width={widthGraph} height={heightGraph} />
             </>
     );
   };
--- src/redux/selectors/statistic.selectors.js
+++ src/redux/selectors/statistic.selectors.js
@@ -3,3 +3,4 @@ export const selectPassedCards = (state) => state.statistic.passedCards;
 export const selectProcentCorrectAnswers = (state) => state.statistic.procentCorrectAnswers;
 export const selectNewWords = (state) => state.statistic.newWords;
 export const selectLongSeriesCorrectAnswers = (state) => state.statistic.longSeriesCorrectAnswers;
+export const selectLearnedWords = (state) => state.statistic.learnedWords;
