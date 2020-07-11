15fbb3dde0e741997d85e06238e6ea26619302cd
Mihail Gorelikov <mTeeeur@gmail.com>
2020-07-12 02:09:22 +0300

    feat: copy savannah

---
+++ src/components/Savannah/UI/buttons.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({ value, number }) => {
+  return (
+    <button className={`btn btn-danger btn-${number}`} type="button">
+      {number} - <span className={`btn-${number}__value`}>{value}</span>
+    </button>
+  );
+};
+
+export default Button;
