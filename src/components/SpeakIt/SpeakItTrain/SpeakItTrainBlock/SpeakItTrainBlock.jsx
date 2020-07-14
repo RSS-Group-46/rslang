fbc8c18f1d4776632b6d84a6fba721d75f6979ba
Liudmila_Arzamasova <liudmila.arzamasova@gmail.com>
2020-07-14 22:29:17 +0300

    feat: implement speech recognition

--- src/components/SpeakIt/SpeakItTrain/SpeakItTrainBlock/SpeakItTrainBlock.jsx
+++ src/components/SpeakIt/SpeakItTrain/SpeakItTrainBlock/SpeakItTrainBlock.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import shortid from 'shortid';
-// import { useSpeechRecognition, useSpeechSynthesis } from 'react-speech-kit';
-import { useSpeechSynthesis } from 'react-speech-kit';
+import { useSpeechRecognition, useSpeechSynthesis } from 'react-speech-kit';
 import Button from '../../UI/Button/Button';
 import defaultTrainImg from '../../assets/images/train-img.jpg';
 import { STATIC_URL, TRANSLATE_URL } from '../../constants/speakItConstants';
@@ -11,15 +10,38 @@ import './SpeakItTrainBlock.scss';
 
 const SpeakItTrainBlock = ({ currentTrainLevel }) => {
   const [words, setWords] = useState([]);
+  const [recognizedText, setRecognizedText] = useState('');
   const [page] = useState(0);
   const [image, setImage] = useState(`${defaultTrainImg}`);
   const [translate, setTranslate] = useState('');
-  const [value, setValue] = useState('');
   const { speak } = useSpeechSynthesis();
+  const { listen, listening, stop } = useSpeechRecognition({
+    onResult: (result) => {
+      console.log('words: ', words);
+      setRecognizedText(result);
+
+      if (words.find((word) => word.word === result)) {
+        const newWords = words.map((word) => {
+          if (word.word === result) {
+            word.guessed = true;
+          }
+          return word;
+        });
+        setWords(newWords);
+      }
+    },
+  });
 
   const wordsURL = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${
     currentTrainLevel - 1
   }`;
+
+  useEffect(() => {
+    if (words.every((word) => word.guessed)) {
+      stop();
+      // and render new a bunch of words
+    }
+  }, [words]);
 
   useEffect(() => {
     axios
@@ -43,19 +65,21 @@ const SpeakItTrainBlock = ({ currentTrainLevel }) => {
 
     const imgUrl = `${STATIC_URL}${word.image}`;
     setImage(imgUrl);
-
-    setValue(word.word);
   };
 
   const wordItem = () => {
     return words.map((word) => {
+      const classes = word.guessed
+        ? 'word-cards__card word-cards__card--guessed'
+        : 'word-cards__card';
       return (
         <li
           key={shortid.generate()}
-          className="word-cards__card"
+          className={classes}
           onClick={() => {
+            stop();
             getWordHandler(word);
-            speak({ text: value });
+            speak({ text: word.word });
           }}
           role="presentation"
         >
@@ -73,11 +97,26 @@ const SpeakItTrainBlock = ({ currentTrainLevel }) => {
   return (
     <div className="train  container">
       <img className="train__img" src={image} alt="train" />
-      <div className="train-word">{translate}</div>
+      <div className="train-word">
+        <div className="train-word__train">{listening ? '' : translate}</div>
+        <div className="train-word__speak">
+          {listening && (
+            <div className="train-word__speak-result">{recognizedText}</div>
+          )}
+        </div>
+      </div>
       <ul className="word-cards">{wordItem()}</ul>
       <div className="train-buttons">
         <Button classes={classesRestart}>Restart</Button>
-        <Button classes={classesSpeak}>Speak please</Button>
+        {listening ? (
+          <Button classes={classesSpeak} onClick={stop}>
+            Stop listening
+          </Button>
+        ) : (
+          <Button classes={classesSpeak} onClick={listen}>
+            Speak please
+          </Button>
+        )}
         <Button classes={classesResult}>Results</Button>
       </div>
     </div>
